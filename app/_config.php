2872b0fbe6c39ccf2a71d53af916b10a5e8958e0
Mateusz Uzdowski <mateusz@silverstripe.com>
2019-09-25 12:09:24 +1200

    Add config.

--- app/_config.php
+++ app/_config.php
@@ -1,5 +1,7 @@
 <?php
 
+use SilverStripe\Core\Environment;
+use SilverStripe\FullTextSearch\Solr\Solr;
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 
@@ -8,3 +10,16 @@ $validator = PasswordValidator::create();
 $validator->setMinLength(8);
 $validator->setHistoricCount(6);
 Member::set_password_validator($validator);
+
+Solr::configure_server(array(
+    'host' => Environment::getEnv('SOLR_SERVER') ?: 'solr',
+    'port' => Environment::getEnv('SOLR_PORT') ?: '8983',
+    'path' => Environment::getEnv('SOLR_PATH') ?: '/solr',
+    'version' => Environment::getEnv('SOLR_VERSION') ?: 4,
+    'indexstore' => array(
+        'mode' => Environment::getEnv('SOLR_MODE') ?: 'file',
+        'auth' => Environment::getEnv('SOLR_AUTH') ?: null,
+        'path' => Environment::getEnv('SOLR_INDEXSTORE_PATH') ?: BASE_PATH.'/.solr',
+        'remotepath' => Environment::getEnv('SOLR_REMOTE_PATH') ?: null,
+    )
+));
